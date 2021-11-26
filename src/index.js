ca9d29ebba0859c75942a6a5ed429d620e16000a
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-11-26 15:30:54 -0300

    stting gh-pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/RecipesApp">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
